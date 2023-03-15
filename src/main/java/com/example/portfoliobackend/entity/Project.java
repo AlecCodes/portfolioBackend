668c1183f678cc6aef08c0f7f5a051b0c0404627
Alec Hannaford <alec322@gmail.com>
2023-03-15 16:08:18 -0700

    id getter and setter

--- src/main/java/com/example/portfoliobackend/entity/Project.java
+++ src/main/java/com/example/portfoliobackend/entity/Project.java
@@ -1,4 +1,24 @@
 package com.example.portfoliobackend.entity;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "PORTFOLIO")
 public class Project {
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "portfolio_generator")
+    @SequenceGenerator(name = "portfolio_generator", sequenceName = "portfolio_seq", allocationSize = 1)
+    private int id;
+
+    private String name;
+
+    private String url;
+
+    private String description;
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+
 }
--- src/main/java/com/example/portfoliobackend/repository/PortfolioRepository.java
+++ src/main/java/com/example/portfoliobackend/repository/PortfolioRepository.java
@@ -2,5 +2,5 @@ package com.example.portfoliobackend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PortfolioRepository extends JpaRepository {
-}
+//public interface PortfolioRepository extends JpaRepository {
+//}
